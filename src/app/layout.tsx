fb547a37bc487b143a4726532456167bb7329f47
Bin-925 <dogok0925@gmail.com>
2026-05-28 14:49:48 +0900

    10강 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Link from "next/link";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-red-500 border-2 border-blue-500`}
-      >
+    <html
+      lang="ko"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <body className="flex flex-col !bg-red-500 border-2 border-blue-500">
         <header>
           <nav className="flex">
             <Link href="/" className="p-2 rounded hover:bg-gray-100">
